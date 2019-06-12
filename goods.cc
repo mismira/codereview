9826f01702709ecbcc082f0046e0daaf6995268c
mismira <mismira.seo@samsung.com>
2019-06-12 15:11:33 +0900

    Update goods.cc

--- goods.cc
+++ goods.cc
@@ -5,13 +5,3 @@ void Goods::SetGoodsInfo(int index, int price) {
   goods_index_ = index;
   goods_price_ = price;
 }
-
-// Return index information
-int Goods::GetIndex() {
-  return goods_index_;
-}
-
-// Return price informaiton
-int Goods::GetPrice() {
-  return goods_price_;
-}
