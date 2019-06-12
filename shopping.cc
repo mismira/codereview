2fa60a45c63e560ea5e13756a1e54b31677822ff
mismira <mismira.seo@samsung.com>
2019-06-12 15:34:35 +0900

    Update shopping.cc

--- shopping.cc
+++ shopping.cc
@@ -20,30 +20,13 @@ void Shopping::PrintList() {
   }
 }
 
+
+
 // Sort shopping_list_ Elements by Price
 // Apply Selection Sort algorithm
-void Shopping::SortByPrice() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
-  std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
-    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
-      if (min == shopping_list_.end()) {
-        min = j;
-        continue;
-      }
-      if (min->GetPrice() > j->GetPrice()) {
-        min = j;
-      }
-    }
-    std::iter_swap(i, min);
-    min = shopping_list_.end();
-  }
-}
 
-// Sort shopping_list_ Elements by Index
-// Apply Selection Sort algorithm
-void Shopping::SortByIndex() {
+void Shopping::SortByPriceOrIndex(int sortType) {
+  
   std::vector<Goods>::iterator p = shopping_list_.begin();
   std::vector<Goods>::iterator min = shopping_list_.end();
   for (std::vector<Goods>::iterator i = shopping_list_.begin();
@@ -53,11 +36,17 @@ void Shopping::SortByIndex() {
         min = j;
         continue;
       }
-      if (min->GetIndex() > j->GetIndex()) {
-        min = j;
+      if (sortType == PRICE){
+        if (min->GetPrice() > j->GetPrice()) {
+            min = j;
+         }
+      }
+      else {
+          if (min->GetIndex() > j->GetIndex()) {
+              min = j;
+          }       
       }
     }
     std::iter_swap(i, min);
     min = shopping_list_.end();
-  }
 }
